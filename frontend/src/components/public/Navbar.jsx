27a3b1c5d905a7c137d92f70864cf8c4f88d27a9
SAMEERSINGHGAUTAM <0808ci211145.ies@ipsacademy.org>
2024-09-19 07:17:56 +0530

    Title modified

--- frontend/src/components/public/Navbar.jsx
+++ frontend/src/components/public/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
             className="h-10 rounded-lg animate-spin"
           />
           <span className="ml-2 text-white font-bold text-3xl">
-            NASA Project
+            Community Mapping
           </span>
         </div>
         <div className="md:hidden">
